1c472a521980855db844d280a43e9771d33b12cd
Pavel P <pavlov.pavel@gmail.com>
2024-03-31 00:26:15 +0200

    Enable asm for 64bit build with msvc using yasm

--- lib/common/portability_macros.h
+++ lib/common/portability_macros.h
@@ -125,6 +125,8 @@
 #  else
 #    define ZSTD_ASM_SUPPORTED 0
 #  endif
+#elif defined(_MSC_VER)
+#  define ZSTD_ASM_SUPPORTED 1
 #else
 #  define ZSTD_ASM_SUPPORTED 0
 #endif
@@ -140,9 +142,8 @@
  *   - DYNAMIC_BMI2 is enabled
  *   - BMI2 is supported at compile time
  */
-#if !defined(ZSTD_DISABLE_ASM) &&                                 \
-    ZSTD_ASM_SUPPORTED &&                                         \
-    defined(__x86_64__) &&                                        \
+#if !defined(ZSTD_DISABLE_ASM) && ZSTD_ASM_SUPPORTED &&           \
+    (defined(__x86_64__) || defined(_M_X64)) &&                   \
     (DYNAMIC_BMI2 || defined(__BMI2__))
 # define ZSTD_ENABLE_ASM_X86_64_BMI2 1
 #else
